2c30e25b94a51b023980471262fc00285f5a57eb
Anzhelika <ana.anajel@yandex.ru>
2022-05-18 18:07:09 +0300

    Изменить кнопки

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,34 @@
 const editButton = document.querySelector(".profile__edit");
 const popup = document.querySelector(".popup");
 const closeButton = popup.querySelector(".popup__close-icon");
-let nameProfile = document.querySelector(".profile__title");
-let job = document.querySelector(".profile__subtitle");
-
-function openСloseForm() {
-  popup.classList.toggle("popup_opened");
-}
-
-editButton.addEventListener("click", openСloseForm);
-closeButton.addEventListener("click", function () {
-  nameInput.value = nameProfile.textContent;
-  jobInput.value = job.textContent;
-  openСloseForm();
-});
-
-const formInput = popup.querySelector(".popup__form");
+const nameProfile = document.querySelector(".profile__title");
+const job = document.querySelector(".profile__subtitle");
+const blockFormInput = popup.querySelector(".popup__form");
 const saveButton = popup.querySelector(".popup__save-button");
 const nameInput = popup.querySelector(".popup__input_type_name");
 const jobInput = popup.querySelector(".popup__input_type_job");
 
-formInput.addEventListener("submit", function () {
-  console.log("Форма отправлена");
-});
+function openСlosePopup() {
+  popup.classList.toggle("popup_opened");
+}
 
-function formSubmitHandler(evt) {
+function handleFormSubmit(evt) {
   evt.preventDefault();
 
   nameProfile.textContent = nameInput.value;
   job.textContent = jobInput.value;
-  openСloseForm();
+  openСlosePopup();
 }
 
-saveButton.addEventListener("click", formSubmitHandler);
+closeButton.addEventListener("click", function () {
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = job.textContent;
+  openСlosePopup();
+});
+
+editButton.addEventListener("click", openСlosePopup);
+saveButton.addEventListener("click", handleFormSubmit);
+
+blockFormInput.addEventListener("submit", function () {
+  console.log("Форма отправлена");
+});
