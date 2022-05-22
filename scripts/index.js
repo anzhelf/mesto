e06e6bc5c5ed93f0ef175ed546e633b48f02c9e2
Anzhelika <ana.anajel@yandex.ru>
2022-05-22 13:14:39 +0300

    исправить порядок в скрипте

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,31 @@
-const editButton = document.querySelector(".profile__edit");
-const popup = document.querySelector(".popup");
-const closeButton = popup.querySelector(".popup__close-icon");
-const nameProfile = document.querySelector(".profile__title");
-const job = document.querySelector(".profile__subtitle");
-const blockFormInput = popup.querySelector(".popup__form");
-const saveButton = popup.querySelector(".popup__save-button");
-const nameInput = popup.querySelector(".popup__input_type_name");
-const jobInput = popup.querySelector(".popup__input_type_job");
+const editButton = document.querySelector(".profile__edit");// Кнопка редактировать
+const popup = document.querySelector(".popup");//попап
+const closeButton = popup.querySelector(".popup__close-icon");// крестик закрыть попап
+const nameProfile = document.querySelector(".profile__title");//Имя на странице
+const job = document.querySelector(".profile__subtitle");//Работа на странице
+const blockFormInput = popup.querySelector(".popup__form");//поля и кнопка формы для ввода
+const saveButton = popup.querySelector(".popup__save-button");//кнопка сохранить
+const nameInput = popup.querySelector(".popup__input_type_name");//поле имя
+const jobInput = popup.querySelector(".popup__input_type_job");//поле работа
 
-function togglePopup() {
-  popup.classList.toggle("popup_opened");
-}
+function togglePopup() {//открыть закрыть попап
+  popup.classList.toggle("popup_opened");//закрыть попап
 
-function handleFormSubmit(evt) {
-  evt.preventDefault();
-
-  nameProfile.textContent = nameInput.value;
-  job.textContent = jobInput.value;
-  togglePopup();
-}
-
-closeButton.addEventListener("click", function () {
-  nameInput.value = nameProfile.textContent;
+  nameInput.value = nameProfile.textContent;//очистить поля ввода
   jobInput.value = job.textContent;
-  togglePopup();
-});
+}
 
-editButton.addEventListener("click", togglePopup);
-saveButton.addEventListener("click", handleFormSubmit);
+function handleFormSubmit(evt) {//отправить форму
+  evt.preventDefault();// не перезагружаем стр.
+
+  nameProfile.textContent = nameInput.value;//инфомация профиля = заполненым полям
+  job.textContent = jobInput.value;
+  togglePopup();//закрываем и обновляем поля
+}
+
+editButton.addEventListener("click", togglePopup);//редактировать - открываем форму и обновляем поля из профиля
+closeButton.addEventListener("click", togglePopup);//закрыть - закрываем форму и стираем поля
+saveButton.addEventListener("click", handleFormSubmit);// сохраняем и обновляем инфу
 
 blockFormInput.addEventListener("submit", function () {
   console.log("Форма отправлена");
