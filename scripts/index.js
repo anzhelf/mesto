55dd5893ad54d90eac3a8c8c861e3a4a454bf9ea
Anzhelika <ana.anajel@yandex.ru>
2022-06-14 11:03:08 +0300

    Найти элементы в дом

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,54 @@ function handleFormSubmit(evt) {
 editButton.addEventListener("click", togglePopup);
 closeButton.addEventListener("click", togglePopup);
 blockFormInput.addEventListener("submit", handleFormSubmit);
+
+//
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const addButton = document.querySelector(".profile__button-add");//кнопка доавить карточку
+//const deletButton = document.querySelector(".")
+const likeButton = document.querySelector(".card__like");
+const card = document.querySelector(".card");
+
+
+function togglePopupAdd() {
+
+  //const popupAdd = document.querySelector("#popup__add").content;//получаем содержимое
+  //const popupElement = popupAdd.querySelector('.popup').cloneNode(true);//клонируем
+  //
+  //userElement.querySelector('.popup__input_type_name').value = "Название";//наполняем содержимым
+  //userElement.querySelector('.popup__input_type_job').value="Ссылка на картинку";
+  //
+  //usersOnline.append(popupElement); //отображаем на стр
+
+  console.log('Нажали на кнопку - функция запустилась');
+
+}
+
+addButton.addEventListener("click", togglePopupAdd);// при нажатии на кнопку открывается попап добавить карточку
