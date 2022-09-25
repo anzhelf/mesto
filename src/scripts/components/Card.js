ebb2338cf23274e8813fc022b53e3fe69fd9da4a
Anzhelika <ana.anajel@yandex.ru>
2022-09-25 15:52:17 +0300

    Исправить ошибки ревью

--- src/scripts/components/Card.js
+++ src/scripts/components/Card.js
@@ -8,15 +8,16 @@ export class Card {
 
 		this._card = settings.card;
 		this._cardImg = settings.cardImage;
-		this._cardLike = settings.cardLike;
+		this._cardLikeSelector = settings.cardLike;
 		this._cardDelete = settings.cardDelete;
 		this._cardTitle = settings.cardTitle;
-		this._templateSelector = templateSelector;
 		this._handleCardClick = handleCardClick;
 		this._handleDeleteClik = handleDeleteClik;
 		this._handlelikeClik = handlelikeClik;
 
 		this._userId = userId;
+
+		this._templateSelector = templateSelector;
 	}
 
 	//достаем разметку
@@ -26,29 +27,10 @@ export class Card {
 	}
 	//слушатели
 	_setEventListeners() {
-		const cardLike = this._element.querySelector(this._cardLike);
-		cardLike.addEventListener('click', () => this._handlelikeClik(this._id));
+		this._cardLike = this._element.querySelector(this._cardLikeSelector);
+		this._cardLike.addEventListener('click', () => this._handlelikeClik(this._id));
 		this._element.querySelector(this._cardDelete).addEventListener('click', () => this._handleDeleteClik(this._id));
 		this._cardImage.addEventListener('click', () => this._handleCardClick(this._text, this._image));
-	}
-
-	//создаем карты
-	generateCard() {
-		this._element = this._getTemplate();
-		this._cardImage = this._element.querySelector(this._cardImg);
-		this._setEventListeners(); // добавим обработчики
-		this._cardImage.src = this._image;
-		this._element.querySelector(this._cardTitle).textContent = this._text;
-		this._cardImage.alt = this._text;
-
-		if (this._ovnerId !== this._userId) {
-			const del = this._element.querySelector(this._cardDelete);
-			del.remove();
-		}
-
-		this.setLikes(this._likes);
-
-		return this._element;
 	}
 
 	//я лайкал?
@@ -60,24 +42,42 @@ export class Card {
 	//Счетчик лайков
 	setLikes(newLikes) {
 		this._likes = newLikes;
-		this._num = this._element.querySelector('.card__like-num');
 		this._num.textContent = this._likes.length;
 		this.handleLikeCard();
 	}
 
 	//если лайкнула я - меняем цвет кнопки
 	handleLikeCard() {
-		const cardLike = this._element.querySelector(this._cardLike);
 		if (this.isLikes()) {
-			cardLike.classList.add('card__like-icon_active');
+			this._cardLike.classList.add('card__like-icon_active');
 		}
 		else {
-			cardLike.classList.remove('card__like-icon_active');
+			this._cardLike.classList.remove('card__like-icon_active');
 		}
 	}
 
 	//удалить карту
 	deleteCard() {
 		this._element.remove();
+	}
+
+	//создаем карты
+	generateCard() {
+		this._element = this._getTemplate();
+		this._cardImage = this._element.querySelector(this._cardImg);
+		this._setEventListeners(); // добавим обработчики
+		this._cardImage.src = this._image;
+		this._element.querySelector(this._cardTitle).textContent = this._text;
+		this._cardImage.alt = this._text;
+		this._num = this._element.querySelector('.card__like-num');
+
+		if (this._ovnerId !== this._userId) {
+			const del = this._element.querySelector(this._cardDelete);
+			del.remove();
+		}
+
+		this.setLikes(this._likes);
+
+		return this._element;
 	}
 }
